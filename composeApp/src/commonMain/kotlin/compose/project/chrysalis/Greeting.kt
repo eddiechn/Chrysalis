81266a4931e444ad581e809930c6798e8ba30bac
Eddie Chen <eddiechn12@gmail.com>
2024-10-15 14:37:27 -0400

    setting up

--- composeApp/src/commonMain/kotlin/compose/project/chrysalis/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package compose.project.chrysalis
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/compose/project/chrysalis/Platform.kt
+++
@@ -1,7 +0,0 @@
-package compose.project.chrysalis
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
